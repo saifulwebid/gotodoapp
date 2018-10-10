e16e8c38b8689f3d16b8702c31d2b53af3c42609
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-10 17:45:46 +0700

    gotodocli: Add urfave/cli.v1 to parse CLI args

--- app/gotodocli/main.go
+++ app/gotodocli/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	app := cli.NewApp()
+
+	app.Name = "gotodocli"
+	app.Usage = "manage your todos"
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Hello world!")
+
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
