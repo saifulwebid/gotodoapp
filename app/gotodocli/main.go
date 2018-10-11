0a8db18694225990b2f0a0694b394180ed977410
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-11 10:30:32 +0700

    gotodocli: Implement getAll

--- app/gotodocli/main.go
+++ app/gotodocli/main.go
@@ -5,23 +5,78 @@ import (
 	"log"
 	"os"
 
+	"github.com/subosito/gotenv"
 	"gopkg.in/urfave/cli.v1"
+
+	"github.com/saifulwebid/gotodo"
+	"github.com/saifulwebid/gotodo/database"
 )
 
+func init() {
+	gotenv.Load()
+}
+
+var db gotodo.Repository
+var service gotodo.Service
+
+func getAll(c *cli.Context) error {
+	todos := service.GetAll()
+
+	fmt.Println(todosToString(todos))
+
+	return nil
+}
+
 func main() {
+	db, err := database.NewRepository()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	service = gotodo.NewService(db)
+
 	app := cli.NewApp()
 
 	app.Name = "gotodocli"
 	app.Usage = "manage your todos"
-
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("Hello world!")
-
-		return nil
+	app.Commands = []cli.Command{
+		{
+			Name:   "getall",
+			Usage:  "get all todos on the database",
+			Action: getAll,
+		},
 	}
 
-	err := app.Run(os.Args)
+	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func todoToString(todo *gotodo.Todo) string {
+	template := `ID: %d
+Title: %s
+Description: %s
+Done: %s
+`
+
+	done := "Pending"
+	if todo.Done {
+		done = "Finished"
+	}
+
+	return fmt.Sprintf(template, todo.ID, todo.Title, todo.Description, done)
+}
+
+func todosToString(todos []*gotodo.Todo) string {
+	ret := ""
+
+	for i, todo := range todos {
+		if i > 0 {
+			ret += "----------------------------\n"
+		}
+		ret += todoToString(todo)
+	}
+
+	return ret
 }
